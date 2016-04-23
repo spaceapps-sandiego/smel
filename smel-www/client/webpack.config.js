26b5cac708163f23df9a92b062778e2a8d831e6e
Slav <njsockpuppet@yahoo.com>
2016-04-23 16:06:05 -0700

    playground

--- smel-www/client/webpack.config.js
+++ smel-www/client/webpack.config.js
@@ -1,31 +1,43 @@
 var path = require('path');
 
+var NODE_PATH = process.env.NODE_PATH || path.resolve(__dirname, './node_modules');
+
 module.exports = {  
+    context: NODE_PATH,
     entry: path.resolve(__dirname, './src/main.js'),
+    resolve: {
+        root: [path.resolve(__dirname, './src'), NODE_PATH]
+    },
+    resolveLoader: {
+        modulesDirectories: [
+            NODE_PATH
+        ]
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
     },
-
+    eslint: {
+        configFile: './.eslintrc.json'
+    },
     module: {
         loaders: [
             // js/jsx
             {
                 test: /src\/.+\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['react', 'react-hmre', 'es2015']
-                }
+                loaders: ['babel?presets[]=react,presets[]=react-hmre,presets[]=es2015', 'eslint-loader']
             },
             // styles
             {
-                test: /src\/styles\/.+\.less$/,
-                exclude: /node_modules/,
+                test: /\.less$/,
                 loader: 'style!css!less'
             },
-            { test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+            // font files
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            }
         ]
     }
 };
