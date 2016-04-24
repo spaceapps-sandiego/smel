27e4a41214d1360d209d905ef23e3d0dde478c8f
Adam <adampk90@gmail.com>
2016-04-23 19:36:14 -0700

    Adding postgres driver

--- smel-www/server/app.js
+++ smel-www/server/app.js
@@ -9,6 +9,7 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import serveFile from './middleware/serve_file';
+import pgCheck from './db';
 
 const BUNDLE_PATH = path.join(__dirname, '../client/dist/bundle.js');
 const INDEX_PATH = path.join(__dirname, '../client/index.html');
@@ -19,10 +20,12 @@ const app = express();
 app.get('/bundle.js', serveFile(BUNDLE_PATH));
 app.get('*', serveFile(INDEX_PATH));
 
-app.listen(PORT, '0.0.0.0', (err) => {
-    if (err) {
-        console.error(err);
-    }
+pgCheck(function() {
+    app.listen(PORT, '0.0.0.0', (err) => {
+        if (err) {
+            console.error(err);
+        }
 
-    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', PORT, PORT);
+        console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', PORT, PORT);
+    });
 });
---
+++ smel-www/server/db.js
@@ -0,0 +1,34 @@
+var pg = require('pg');
+const {POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DB} = process.env;
+
+var connString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}`;
+var check_count = 0;
+
+//connects to the database
+var pgCheck = function(cb) {
+    pg.connect(connString, function(error, client, done) {
+        if (error) {
+            if (check_count++ < 10) {
+                setTimeout(function() {
+                    pgCheck(cb);
+                }, 5000);
+            }
+
+            return console.error("ERROR connecting to postgres database", error);
+        }
+
+        //run a stupid test query to check the database connection
+        client.query("SELECT $1::int AS number", ["1"], function(error, result) {
+            done();
+
+            if (error) {
+                return console.error("ERROR running query", error);
+            }
+
+            console.log("Postgres database check succeeded");
+            cb();
+        });
+    });
+};
+
+export default pgCheck;
